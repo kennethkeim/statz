dfb666f3f0175f430d4b222234abf3cc61ae5d9f
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-05-04 12:38:07 -0400

    Refactor page to show metrics I care about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import { mockStravaActivities } from "./_data/strava-activities";
 // import type { StravaActivity } from "./_models/strava-activities.model";
 
+const METERS_IN_MILE = 1609.344;
+const NUM_FASTEST_RESULTS = 5;
+
+const roundTo2 = (num: number): number => {
+  // apparently this isn't bulletproof - https://stackoverflow.com/a/12830454/8935239
+  return Math.round((num + Number.EPSILON) * 100) / 100;
+};
+
+const metersPerSecToMinPerMile = (metersPerSec: number): string => {
+  const minPerMile = 26.8224 / metersPerSec;
+  const min = Math.floor(minPerMile);
+  const sec = Math.floor((minPerMile - min) * 60);
+  return `${min}:${sec.toString().padStart(2, "0")}`;
+};
+
 export default async function Home() {
   console.log("getting data from Strava...");
 
@@ -27,25 +42,35 @@ export default async function Home() {
     <main className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] p-6 text-white">
       <h1 className="mb-6">So. You want to see some Strava Statz.</h1>
 
-      <h2 className="mb-3">Fastest runs</h2>
+      <h2 className="mb-3 text-xl">{`Top ${NUM_FASTEST_RESULTS} fastest miles`}</h2>
+      <p className="mb-3 text-sm text-slate-400">
+        (based on avg of entire run, not based on splits)
+      </p>
       <ul>
         {[...activities]
-          .sort((a, b) => b.average_speed - a.average_speed)
           .filter(
             (activity) =>
-              activity.type === "Run" && activity.average_speed > 2.8,
+              activity.type === "Run" && activity.distance >= METERS_IN_MILE,
           )
-          .map((activity) => (
-            <li key={activity.id} className="mb-3 flex justify-between">
-              <p>{activity.start_date}</p>
-              <p>
-                {/* {activity.distance}m in {activity.moving_time}s */}
-                {/* {activity.average_heartrate}bpm */}
-                {activity.average_speed}
-                {/* {activity.average_speed} */}
-              </p>
-            </li>
-          ))}
+          .sort((a, b) => b.average_speed - a.average_speed)
+          .slice(0, NUM_FASTEST_RESULTS)
+          .map((activity) => {
+            const date = new Date(activity.start_date);
+            const month = (date.getMonth() + 1).toString().padStart(2, "0");
+            const day = date.getDate().toString().padStart(2, "0");
+            const dateString = `${date.getFullYear()}/${month}/${day}`;
+            return (
+              <li key={activity.id} className="mb-3 flex justify-between">
+                <p>{dateString}</p>
+                <p>{roundTo2(activity.distance / METERS_IN_MILE)} miles</p>
+                <p>
+                  {/* {activity.distance}m in {activity.moving_time}s */}
+                  {/* {activity.average_heartrate}bpm */}
+                  {metersPerSecToMinPerMile(activity.average_speed)}
+                </p>
+              </li>
+            );
+          })}
       </ul>
     </main>
   );
